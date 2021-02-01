bf7c8be7af4f310e59f61bf530163dce2464a909
Alejandro <alejandrog.gorosito@gmail.com>
2021-02-01 03:34:12 -0300

    fix pg connection

--- src/database/connection.js
+++ src/database/connection.js
@@ -5,7 +5,7 @@ const connection = {
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
     port: 5432,
-    username: process.env.DB_USER,
+    user: process.env.DB_USER,
     password: process.env.DB_PASS
 };
 
